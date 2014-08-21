059e9dce97a3ef7324fa5a274087f42d81bd19ae
Vallerious <valericfbg@gmail.com>
2014-08-21 22:58:52 +0300

    implementing avatar uploading

--- php/create_new_user.php
+++ php/create_new_user.php
@@ -24,18 +24,32 @@
 		if ($password != $repassword) {
 		    $message = "Your password didn't match! Please try again.";
 		} elseif ( empty($errors) ) {
-			$query = "INSERT INTO users (
-							username, password, email
-						) VALUES (
-							'{$username}', '{$hashed_password}', '{$email}'
-						)";
-			$result = mysql_query($query, $connection);
-			if ($result) {
-				$message = "The user was successfully created.";
-			} else {
-				$message = "The user could not be created.";
-				$message .= "<br />" . mysql_error();
-			}
+            $file = $_FILES['image']['tmp_name'];
+            if(!isset($file)) {
+                echo "Please select a file to upload";
+            } else {
+                if(!empty($_FILES['image']['name'])) {
+                    $image = addslashes(file_get_contents($_FILES['image']['tmp_name']));
+                    $image_name = addslashes($_FILES['image']['name']);
+                    $image_size = getimagesize($_FILES['image']['tmp_name']);
+                    echo $image_size;
+                    $query = "INSERT INTO users (
+                                    username, password, email, image
+                                ) VALUES (
+                                    '{$username}', '{$hashed_password}', '{$email}', '{$image}'
+                                )";
+                    $result = mysql_query($query, $connection);
+                    if ($result) {
+                        $message = "The user was successfully created.";
+                    } else {
+                        $message = "The user could not be created.";
+                        $message .= "<br />" . mysql_error();
+                    }
+                } else {
+                    echo "Please upload an image";
+                }
+
+            }
 		} else {
 			if (count($errors) == 1) {
 				$message = "There was 1 error in the form.";
@@ -43,6 +57,9 @@
 				$message = "There were " . count($errors) . " errors in the form.";
 			}
 		}
+
+
+
 	} else { // Form has not been submitted.
 		$username = "";
 		$password = "";
@@ -60,8 +77,8 @@
         <body>
 	    <?php if (!empty($message)) {echo "<p class=\"message\">" . $message . "</p>";} ?>
 	    <?php if (!empty($errors)) { display_errors($errors); } ?>
-            <form method = "post">
-                <p>Fill This Form Bellow</p>
+            <form method = "post" enctype="multipart/form-data">
+                <p>Fill This Form Below</p>
                 <input type = "text" name = "username" placeholder = "User name" required="true"
 		       title="The field is empty, please enter your login name" />
                 <input type = "password" name = "password" placeholder = "Enter Your Password" required="true"
@@ -71,7 +88,10 @@
 		<input type = "email" name = "email" placeholder = "Enter Your Email" required="true"
 		       pattern="[a-zA-Z0-9_]{3,}@[a-zA-Z0-9_]{3,}.[a-zA-Z0-9_]{2,4}"
 		       title="Please enter a valid email address"/>
-                <input type = "submit" name = "submition" value = "Submit" />
+                <input type = "submit" name = "submition" value = "Submit" /> <br/>
+                    File:
+                    <input type="file" name="image"/><br/>
+                <br/>
 		<a href="index.php">Back to the main page</a><br />
             </form>
         </body>
