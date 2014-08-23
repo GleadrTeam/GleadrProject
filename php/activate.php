cc2da435da6f9e7fa180686c4adde0e48f3bf0bc
Vallerious <valericfbg@gmail.com>
2014-08-23 15:29:29 +0300

    email verification, input validation

---
+++ php/activate.php
@@ -0,0 +1,19 @@
+<?php
+include("Includes/form_functions.php");
+
+if(isset($_GET['aid'])) {
+    activateAccount($_GET['aid']);
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Activation Page</title>
+</head>
+<body>
+<p> Your account has been activated </p>
+<hr/>
+<a href="index.php">Back to the main page</a><br />
+</body>
+</html>	
